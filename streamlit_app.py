40db7642e90188ee5eff1ba8923d08d937d3b281
rj126 <rjagetia96@gmail.com>
2024-03-07 14:39:32 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ name1 = st.text_input("Enter the first name:")
 name2 = st.text_input("Enter the second name:")
 
 # Button to check for match (without any action)
-if st.button("Check for Match"):
+if st.button("Check for a Match"):
     pass
