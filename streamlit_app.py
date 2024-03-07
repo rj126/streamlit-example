2a6b9a729a5c7e9cff299ec9db28a5d0a9e04214
rj126 <rjagetia96@gmail.com>
2024-03-07 14:01:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,10 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+"""
 # OpenAI API key
 openai.api_key = "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"
+"""
 
 # Title
 st.title("Name Matching App")
