0a3c20067481c12fbd8e29096f04a622f4a47873
rj126 <rjagetia96@gmail.com>
2024-03-07 14:03:58 -0800

    Update streamlit_app.py
    remove "you entered" bit

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,3 @@ name2 = st.text_input("Enter the second name:")
 # Button to check for match (without any action)
 if st.button("Check for Match"):
     pass
-
-# Display the entered names
-st.write("You entered the following names:")
-st.write(f"Name 1: {name1}")
-st.write(f"Name 2: {name2}")
-
-
